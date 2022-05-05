0f88b6e6117b42cfb733264ed7b993f48444d1ed
Aranesh <a.aranesh@gmail.com>
2022-05-05 17:03:36 -0400

    Reading in files

--- read.py
+++ read.py
@@ -1,6 +1,18 @@
 import cv2 as cv  
 
-img = cv.imread('Photos/cat.jpg') 
-cv.imshow('Cat', img)  
+#img = cv.imread('Photos/cat.jpg') 
+#cv.imshow('Cat', img)  
 
-cv.waitKey(0)
+#Reading in image  
+capture = cv.VideoCapture('videos/dog.mp4') 
+
+while True: 
+    isTrue, frame = capture.read() 
+    cv.imshow('Video',frame) 
+
+
+    if cv.waitKey(20) & 0xFF==ord('d'): 
+        break 
+
+capture.release()
+cv.destroyAllWindows()
